320986261ce01d571056142521c1e7965151a106
Ariful Haque <arifulh145@gmail.com>
2025-04-29 21:18:01 +0600

    firebase and auth setup successfully

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useReducer } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  loading: true,
+};
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload, loading: false };
+    case "LOGOUT":
+      return { ...state, user: null, loading: false };
+    case "SET_LOADING":
+      return { ...state, loading: true };
+    default:
+      return state;
+  }
+}
+
+export function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch({ type: "LOGIN", payload: user });
+      } else {
+        dispatch({ type: "LOGOUT" });
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYeSiPjMXB7m8fEn3E_JQnZqyxE5AzLSI",
+  authDomain: "foodify-f8859.firebaseapp.com",
+  projectId: "foodify-f8859",
+  storageBucket: "foodify-f8859.firebasestorage.app",
+  messagingSenderId: "985503354430",
+  appId: "1:985503354430:web:1e30e8f94234ab5c8c8b6f",
+};
