7e3a4d73a375dd77dae030e2b035d2d61f77f5f7
Ariful Haque <arifulh145@gmail.com>
2025-04-29 16:38:22 +0600

    foodify client app created successfully

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
